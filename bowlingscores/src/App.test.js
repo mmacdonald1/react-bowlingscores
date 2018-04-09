1a2763277ad3fb462dcf150c129df93201c20094
Melanie MacDonald <melmacd94@gmail.com>
2018-04-09 19:56:49 -0400

    testing finally working

--- bowlingscores/src/App.test.js
+++ bowlingscores/src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import {mount} from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App',()=>{
+  let app = mount (<App />);
+
+  it('renders the App title', () => {
+    // console.log(app.debug());
+    expect(app.find('h2').text()).toEqual('Bowling Scores App')
+  });
 });
