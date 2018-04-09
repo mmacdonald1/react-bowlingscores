dc96a3bd855a90e2d1d228ea9d18b90a9720ff51
Melanie MacDonald <melmacd94@gmail.com>
2018-04-09 17:53:58 -0400

    config for testing

--- bowlingscores/src/App.js
+++ bowlingscores/src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
 
     this.state = {
       text: '',
-      rolls: []
+      rolls: [],
+      result: 0,
+      rollIndex: 0
     }
   }
   onChange = (e) => {
@@ -19,8 +21,55 @@ class App extends Component {
     const text = this.state.text;
     let rolls = text.replace(/[-]/g,"", "").split("");
     this.setState({rolls: rolls});
-
+    // this.score()
   }
+
+  // score(){
+  //   const {result, rollIndex} = this.state;
+  //
+  //     for (let frameIndex = 0; frameIndex< 10; frameIndex++){
+  //       //accounting for a spare
+  //       if(this.isStrike()){
+  //         this.setState{(result: this.state.result += this.getStrikeScore())};
+  //         rollIndex++
+  //       }
+  //       else if (this.isSpare()){
+  //         result += this.getSpareScore();
+  //         //gives the two rolls in each frame
+  //         rollIndex += 2;
+  //       }
+  //       else{
+  //         result += this.getNormalScore();
+  //         //gives the two rolls in each frame
+  //         rollIndex += 2;
+  //       }
+  //
+  //     }
+  //           return result;
+  //
+  // }
+  // isStrike(){
+  //   return this.rolls[rollIndex] == 'X';
+  // }
+  //
+  // isSpare(){
+  //   return this.rolls[rollIndex] == '/';
+  // }
+  //
+  // getStrikeScore(){
+  //   return 10 + this.rolls[rollIndex + 1];
+  // }
+  //
+  // getSpareScore(){
+  //   return 10 + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+  // }
+  //
+  // getNormalScore(){
+  //   return this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+  // }
+  //
+
+
 
   render() {
     return (
